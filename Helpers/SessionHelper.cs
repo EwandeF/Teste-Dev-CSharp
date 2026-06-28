3f0c5b4778f3eec766b8166f8fe03e73201c750e
Ewander Felix Ferreira <ewander76@gmail.com>
2026-06-27 21:32:15 -0300

    feat: adicionar helpers ao case

---
+++ Helpers/SessionHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace TesteDevCSharp.Helpers
+{
+    public static class SessionHelper
+    {
+        public static int? GetUsuarioId(IHttpContextAccessor httpContextAccessor)
+        {
+            var id = httpContextAccessor.HttpContext?.Session.GetString("UsuarioId");
+            if (id == null) return null;
+            return int.Parse(id);
+        }
+
+        public static void SetUsuario(IHttpContextAccessor httpContextAccessor, int id, string nome)
+        {
+            httpContextAccessor.HttpContext?.Session.SetString("UsuarioId", id.ToString());
+            httpContextAccessor.HttpContext?.Session.SetString("UsuarioNome", nome);
+        }
+
+        public static void Limpar(IHttpContextAccessor httpContextAccessor)
+        {
+            httpContextAccessor.HttpContext?.Session.Clear();
+        }
+    }
+}
